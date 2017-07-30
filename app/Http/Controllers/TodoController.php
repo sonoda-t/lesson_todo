3a7b03ce7579b29be11fb42f7fbe4c5e9f0a7b7e
園田崇人 <tri.sonotaka@gmail.com>
2017-07-30 23:52:18 +0900

    Todo更新の実装

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -27,12 +27,21 @@ class TodoController extends Controller
 	public function create(){
 		return view('todo.create');
 	}
-	public function store(Request $request)
-	{
-    		$input =  $request->all();
+	public function edit($id){
+                $todo = $this->todo->find($id);
+                return view('todo.edit')->with(compact('todo'));
+        }
+	public function store(Request $request){
+    		$input = $request->all();
     		$this->todo->fill($input);
     		$this->todo->save();
 
     		return redirect()->to('todo');
+	}
+	public function update(Request $request, $id){
+		$input = $request->all();
+		$this->todo->where('id', $id)->update(['title' => $input['title']]);
+
+		return redirect()->to('todo');
 	}
 }
