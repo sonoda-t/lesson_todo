e2198130f0b46c0e98f35368c451dfb7b5976ad1
園田崇人 <tri.sonotaka@gmail.com>
2017-08-01 21:32:00 +0900

    gulp適用及び修正

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -8,43 +8,56 @@ use App\Http\Requests;
 
 class TodoController extends Controller
 {
-	
+
 	private $todo;
 
-	public function __construct(Todo $todo){
+	public function __construct(Todo $todo)
+	{
 		$this->todo = $todo;
 	}
-	
+
 	/**
 	* Display a listing of the resource.
 	*
 	* @return Response
 	*/
-	public function index(){
+
+	public function index()
+	{
 		$todos = $this->todo->all();
 		return view('todo.index', compact('todos'));
 	}
-	public function create(){
+
+	public function create()
+	{
 		return view('todo.create');
 	}
-	public function edit($id){
-                $todo = $this->todo->find($id);
-                return view('todo.edit')->with(compact('todo'));
-        }
-	public function store(Request $request){
-    		$input = $request->all();
-    		$this->todo->fill($input);
-    		$this->todo->save();
 
-    		return redirect()->to('todo');
+	public function edit($id)
+	{
+    $todo = $this->todo->find($id);
+    return view('todo.edit')->with(compact('todo'));
 	}
-	public function update(Request $request, $id){
+
+	public function store(Request $request)
+	{
+		$input = $request->all();
+		$this->todo->fill($input);
+		$this->todo->save();
+
+		return redirect()->to('todo');
+	}
+
+	public function update(Request $request, $id)
+	{
 		$input = $request->all();
 		$this->todo->where('id', $id)->update(['title' => $input['title']]);
 
 		return redirect()->to('todo');
 	}
-	public function destroy($id){
+
+	public function destroy($id)
+	{
 		$data = $this->todo->find($id);
 		$data->delete();
 
